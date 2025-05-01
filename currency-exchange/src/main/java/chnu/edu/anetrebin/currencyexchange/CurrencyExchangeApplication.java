72c51d74baa13ca8bc031fb04838d284cd945901
Arthur Netrebin <artur.netrebin@gmail.com>
2025-05-01 11:27:26 +0300

    Added Eureka Discovery Server and its settings

--- currency-exchange/src/main/java/chnu/edu/anetrebin/currencyexchange/CurrencyExchangeApplication.java
+++ currency-exchange/src/main/java/chnu/edu/anetrebin/currencyexchange/CurrencyExchangeApplication.java
@@ -2,8 +2,10 @@ package chnu.edu.anetrebin.currencyexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeApplication {
 
     public static void main(String[] args) {
---
+++ eureka-server/src/test/java/chnu/edu/anetrebin/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package chnu.edu.anetrebin.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
